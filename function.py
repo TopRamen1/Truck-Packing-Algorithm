9b86de14dce2a20182a038f540635504992730de
baradejoo <baradziejkamil@gmail.com>
2020-11-01 02:30:39 +0100

    Changes in function.py (reading data from file)

--- function.py
+++ function.py
@@ -1,7 +1,18 @@
 
 def get_address_id_weight_package(filename):
-    f = open(filename, "r")
-    for x in f:
-        address_id_weight_package = {2: [x, 4]}
-    return address_id_weight_package
+    num=[]
+    with open(filename, "r") as reader:
+        for line in reader.readlines():
+            for word in line.split('\n'):
+                for in_word in word.split(' '):
+                    print(in_word)
+                    if in_word.isdigit():
+                        num.append(int(in_word))
 
+    print(num)
+
+
+
+
+if __name__ == '__main__':
+    get_address_id_weight_package("data_1.txt")
