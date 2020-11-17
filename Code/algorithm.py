ba6c11efcb3cc218ba3626e185cfe53260c2e2f0
baradejoo <baradziejkamil@gmail.com>
2020-11-17 15:07:35 +0100

    algorithm implementation

---
+++ Code/algorithm.py
@@ -0,0 +1,30 @@
+import main.py
+class Individual:
+    def __init__(self ):
+        pass
+
+
+def genetic_alg():
+    """    """
+    pass
+
+def init_pop():
+    """ Initialize population """
+    pass
+
+def fitness():
+    pass
+
+def crossover():
+    pass
+
+def mutation():
+    pass
+
+def selection():
+    pass
+
+
+
+
+
--- Code/functions.py
+++ Code/functions.py
@@ -1,18 +1,19 @@
+#
 
-def get_package_data(filename):
-    num = []
-    with open(filename, "r") as reader:
-        for line in reader.readlines():
-            for word in line.split('\n'):
-                for in_word in word.split(' '):
-                    print(in_word)
-                    if in_word.isdigit():
-                        num.append(int(in_word))
-
-    print(num)
-
+# def get_package_data(filename):
+#     num = []
+#     with open(filename, "r") as reader:
+#         for line in reader.readlines():
+#             for word in line.split('\n'):
+#                 for in_word in word.split(' '):
+#                     print(in_word)
+#                     if in_word.isdigit():
+#                         num.append(int(in_word))
+#
+#     print(num)
+#
 
 
 
 if __name__ == '__main__':
-    get_package_data("data_1.txt")
+
