5660f43dc892db6fec0855e036ff835e40a81bd4
baradejoo <baradziejkamil@gmail.com>
2020-11-22 18:24:53 +0100

    algorithm_data + algorithm (obj_fcn) v2

--- Code/algorithm_data.py
+++ Code/algorithm_data.py
@@ -1,21 +1,20 @@
 from typing import List, Tuple
-import numpy as np
-from copy import deepcopy
 
 
 class DataFromFile:
-    def __init__(self, filename_1: str, filename_2: str, filename_3: str):
-        self.filename1 = filename_1
-        self.filename2 = filename_2
-        self.filename3 = filename_3
-        #self.id_dataset = id_dataset_[-1]
+    def __init__(self, filename_p: str, filename_t: str, filename_s: str, id_dataset_: List[int]):
+        self.filename1 = filename_p
+        self.filename2 = filename_t
+        self.filename3 = filename_s
+        self.id_dataset = id_dataset_[-1]
 
-    #def __str__(self) -> str:
-        #return f"ID of dataset: {self.id_dataset} -> Name: '{self.filename}'".format(self=self)
+    def __str__(self) -> str:
+        return f"ID of dataset: {self.id_dataset} -> Names: '{self.filename1},'{self.filename2}','{self.filename3}'" \
+            .format(self=self)
 
-    #@classmethod
-    #def from_file_id(cls, filename_1):
-        #return cls(filename_1, [int(i) for i in filename_1 if i.isdigit()])
+    @classmethod
+    def from_file_id(cls, *args):
+        return cls(*args, [int(i) for i in args[0] if i.isdigit()])
 
     def get_package_data(self) -> List[Tuple[int, int, float]]:
         """ Extract data from .txt file into list of tuples, tuples contain information about packages: ID,
@@ -67,8 +66,7 @@ class Package:
         self.weight = weight_
 
     def __str__(self) -> str:
-        return f"Package no. {self.id}, Address: {self.address}, Weight: {self.weight}".format(
-            self=self)
+        return f"Package no. {self.id}, Address: {self.address}, Weight: {self.weight}".format(self=self)
 
     @property
     def info_package(self):
@@ -76,7 +74,8 @@ class Package:
 
 
 class Truck:
-    def __init__(self, id_: int, type_t_: str, load_: float, exp_cost_: float, min_fuel_use_: float, max_fuel_use_: float):
+    def __init__(self, id_: int, type_t_: str, load_: float, exp_cost_: float, min_fuel_use_: float,
+                 max_fuel_use_: float):
         self.id = id_
         self.type_t = type_t_
         self.load = load_
@@ -91,7 +90,7 @@ class Truck:
     @property
     def info_truck(self):
         return f"Truck no. {self.id}, Type: {self.type_t}, Load: {self.load}, Exploitation: {self.exp_cost}, Min. " \
-            f"combustion: {self.min_fuel_use}, Max. combustion: {self.max_fuel_use}".format(self=self)
+               f"combustion: {self.min_fuel_use}, Max. combustion: {self.max_fuel_use}".format(self=self)
 
 
 class Storage:
@@ -110,32 +109,28 @@ class Storage:
 
 class MainStorage:
     def __init__(self, data_init_: DataFromFile):
-        data_init = data_init_
-        data_package_ = data_init.get_package_data()
-        truck_package_ = data_init.get_truck_data()
-        storage_package_ = data_init.get_storage_data()
         self.list_of_packages = []
         self.list_of_trucks = []
         self.list_of_storages = []
-        self.k = 4.5 # fuel price
-        for i in data_package_:
-            new_package = Package(i[0], i[1], i[2])
-            self.list_of_packages.append(new_package)
-        for i in truck_package_:
-            new_truck = Truck(i[0], i[1], i[2], i[3], i[4], i[5])
-            self.list_of_trucks.append(new_truck)
-        for i in storage_package_:
-            new_storage = Storage(i[0], i[1], i[2])
-            self.list_of_storages.append(new_storage)
+        self.k = 4.5  # fuel price
+
+        for i in data_init_.get_package_data():
+            self.list_of_packages.append(Package(i[0], i[1], i[2]))
+
+        for i in data_init_.get_truck_data():
+            self.list_of_trucks.append(Truck(i[0], i[1], i[2], i[3], i[4], i[5]))
+
+        for i in data_init_.get_storage_data():
+            self.list_of_storages.append(Storage(i[0], i[1], i[2]))
 
     def __iter__(self):
         return iter(self.list_of_packages)
 
     def __str__(self):
-        return f"Number of packages: {len(self.list_of_packages)}\nNumber of trucks: {len(self.list_of_trucks)}\nNumber of storages: {len(self.list_of_storages)}".format(self=self)
+        return f"Number of packages: {len(self.list_of_packages)}\nNumber of trucks: {len(self.list_of_trucks)}\n" \
+               f"Number of storages: {len(self.list_of_storages)}".format(self=self)
 
     @property
     def info_main_storage(self):
-        return f"Number of packages: {len(self.list_of_packages)}\nNumber of trucks: {len(self.list_of_trucks)}\nNumber of storages: {len(self.list_of_storages)}".format(self=self)
-
-
+        return f"Number of packages: {len(self.list_of_packages)}\nNumber of trucks: {len(self.list_of_trucks)}\n" \
+               f"Number of storages: {len(self.list_of_storages)}".format(self=self)
