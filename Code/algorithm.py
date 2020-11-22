8188316c7b7baa36aad351334b9361f8443f8077
TopRamen1 <nicolas.duc@op.pl>
2020-11-22 14:46:00 +0100

    init_pop fix

--- Code/algorithm.py
+++ Code/algorithm.py
@@ -14,6 +14,9 @@ class Individual:
         self.ch_t = ch_t
         self.ch_p = ch_p
 
+    def __str__(self):
+        return str(self.ch_t) + ' ' + str(self.ch_p)
+
 
 def genetic_alg(main_storage: MainStorage):
     """    """
@@ -30,28 +33,21 @@ def chcek_lims(data: MainStorage):
     return True
 
 
-
 def random_chromosome(data: MainStorage):
     """ Generates a random Chromosome for individual """
+    # id lists for calculations
     storage_ids = list(range(0, len(data.list_of_storages)))
     truck_ids = list(range(0, len(data.list_of_trucks)))
     package_ids = list(range(0, len(data.list_of_packages)))
-
-    p_len = len(package_ids)
-    weight_sum = 0
 
     # init chromosome
     ch_t = [-1] * len(data.list_of_trucks)
     ch_p = [-1] * len(data.list_of_packages)
 
-    # sorted packages by id
-    #ids_by_address = [[]] * len(storage_ids)
+    # sorted packages by address
     ids_by_address = [[] for i in range(len(storage_ids))]
-    print(ids_by_address)
     for p_id in package_ids:
         ids_by_address[data.list_of_packages[p_id].address].append(p_id)
-
-    print(ids_by_address)
 
     while package_ids:
         for p_to_add in ids_by_address:
@@ -60,22 +56,25 @@ def random_chromosome(data: MainStorage):
                 return [0], [0]
 
             t = data.list_of_trucks[random.choice(truck_ids)]  # random truck
-            truck_ids.pop(t.id)
-
-            weight_sum = 0
+            truck_ids.remove(t.id)
 
             p = data.list_of_packages[random.choice(p_to_add)]  # random package
-            ch_t[t.id] = p.address
 
+            ch_t[t.id] = p.address  # adding truck address to chromosome
+
+            weight_sum = 0
             while t.load >= weight_sum + p.weight:
                 weight_sum += p.weight
-                ch_p[p.id] = t.id
 
-                package_ids.pop(p.id)
-                p_to_add.pop(p.id)
+                ch_p[p.id] = t.id   # adding truck id for package in chromosome
+
+                package_ids.remove(p.id)
+                p_to_add.remove(p.id)
 
                 if p_to_add:
                     p = data.list_of_packages[random.choice(p_to_add)]
+                else:
+                    break
 
     return ch_t, ch_p
 
--- Code/main.py
+++ Code/main.py
@@ -3,13 +3,29 @@ from algorithm_data import MainStorage
 from algorithm import init_pop
 
 if __name__ == '__main__':
-    data = DataFromFile("data/data_test1_p.txt", "data/data_test1_t.txt", "data/data_test1_s.txt")
+    data = DataFromFile("data/test1/p.txt", "data/test1/t.txt", "data/test1/s.txt")
 
     storage = MainStorage(data)
 
     print(storage)
 
     pop = init_pop(storage, 3)
+
+    for i in pop:
+        print(i)
+
+    data = DataFromFile("data/test2/p.txt", "data/test2/t.txt", "data/test2/s.txt")
+
+    storage = MainStorage(data)
+
+    print(storage)
+
+    pop = init_pop(storage, 3)
+
+    for i in pop:
+        print(i)
+
+
 
 
 
